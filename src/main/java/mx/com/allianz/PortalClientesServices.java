31564c7c37133699fc2a0964169d210af90c7512
BLUEIT\francisco.gonzalez <francisco.gonzalez@qaracter.com>
2025-04-07 11:25:51 -0600

    Se sube primera versión del arquetipo de portal clientes

---
+++ src/main/java/mx/com/allianz/PortalClientesServices.java
@@ -0,0 +1,13 @@
+package mx.com.allianz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalClientesServices {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalClientesServices.class, args);
+	}
+
+}
